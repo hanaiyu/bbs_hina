7f3675598e47b51ba5cd86f918616d6695d7b1ed
yuta.hanai <(hanaiyu22@gmail.com)>
2019-11-12 12:00:53 +0900

    jsの記述を削除 assetフォルダ追加

--- public_html/js/bbs.js
+++ public_html/js/bbs.js
@@ -1,27 +1,2 @@
 $(function () {
-  // アップロードするファイルを選択
-  $('input[type=file]').change(function () {
-    var file = $(this).prop('files')[0];
-
-    // 画像以外は処理を停止
-    if (!file.type.match('image.*')) {
-      // クリア
-      $(this).val('');
-      $('.imgarea').html('');
-      return;
-    }
-
-    // 画像表示
-    var reader = new FileReader();
-    reader.onload = function () {
-      var img_src = $('<img>').attr('src', reader.result);
-      $('.imgarea').html(img_src);
-    }
-    reader.readAsDataURL(file);
-  });
-
-  $('.fav__btn').on('click', function () {
-    $favbtn = $(this);
-    $($favbtn).toggleClass('active');
-  });
 });
